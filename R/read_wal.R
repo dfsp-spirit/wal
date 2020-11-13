eefe7a145cb6513ad50b38281638db081137a300
Tim Schäfer <ts+code@rcmd.org>
2020-11-13 17:20:14 +0100

    sketvch wal reading func

--- R/read_wal.R
+++ R/read_wal.R
@@ -20,7 +20,7 @@
 #' }
 #'
 #' @export
-read.wal <- function(filepath, hdr = TRUE, hdr_only = FALSE) {
+read.wal <- function(filepath, hdr = TRUE, hdr_only = FALSE, num_mip_maps = 4L) {
   fh = file(filepath, "rb");
   on.exit({ close(fh) });
 
@@ -28,6 +28,15 @@ read.wal <- function(filepath, hdr = TRUE, hdr_only = FALSE) {
 
   wal = list();
   header = list();
+
+  header$tex_name = readChar(fh, 32L);
+  header$width = readBin(fh, integer(), n = 1, size = 1, signed = FALSE, endian = endian);
+  header$height = readBin(fh, integer(), n = 1, size = 1, signed = FALSE, endian = endian);
+  header$mip_level_offsets = readBin(fh, integer(), n = num_mip_maps, size = 1, signed = FALSE, endian = endian);
+  header$anim_name = readChar(fh, 32L);  # next frame in animation.
+  header$flags = readBin(fh, integer(), n = 1, size = 4, endian = endian);
+  header$contents = readBin(fh, integer(), n = 1, size = 4, endian = endian);
+  header$value = readBin(fh, integer(), n = 1, size = 4, endian = endian);
 
   header$ident = readBin(fh, integer(), n = 1, size = 1, endian = endian);
   if(header$ident != 10L) {
