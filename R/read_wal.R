34ee16679dfbb7278da22d00da4adb150dcd7539
Tim Schäfer <ts+code@rcmd.org>
2020-11-14 11:34:55 +0100

    spec

--- R/read_wal.R
+++ R/read_wal.R
@@ -32,16 +32,18 @@ read.wal <- function(filepath, hdr = TRUE, hdr_only = FALSE, num_mip_maps = 4L) 
   header = list();
 
   header$tex_name = readChar(fh, 32L);
-  header$width = readBin(fh, integer(), n = 1, size = 1, signed = FALSE, endian = endian);
-  header$height = readBin(fh, integer(), n = 1, size = 1, signed = FALSE, endian = endian);
-  header$mip_level_offsets = readBin(fh, integer(), n = num_mip_maps, size = 1, signed = FALSE, endian = endian);
+  #header$tex_name = read.fixed.char.binary(fh, 32L);
+  header$width = readBin(fh, integer(), n = 1, size = 2, signed = FALSE, endian = endian);
+  header$height = readBin(fh, integer(), n = 1, size = 2, signed = FALSE, endian = endian);
+  header$mip_level_offsets = readBin(fh, integer(), n = num_mip_maps, size = 2, signed = FALSE, endian = endian);
   header$anim_name = readChar(fh, 32L);  # next frame in animation.
+  #header$anim_name =read.fixed.char.binary(fh, 32L);
   header$flags = readBin(fh, integer(), n = 1, size = 4, endian = endian);
   header$contents = readBin(fh, integer(), n = 1, size = 4, endian = endian);
   header$value = readBin(fh, integer(), n = 1, size = 4, endian = endian);
 
   if(header$width < 1L | header$height < 1L) {
-    stop("File not in WAL format (or invalid zero-length image dimension).");
+    warning("File not in WAL format (or invalid zero-length image dimension).");
   }
 
   # Read data for all mimaps.
@@ -52,3 +54,8 @@ read.wal <- function(filepath, hdr = TRUE, hdr_only = FALSE, num_mip_maps = 4L) 
   return(wal);
 }
 
+#' @keywords internal
+read.fixed.char.binary <- function(filehandle, n, to = "UTF-8") {
+  txt = readBin(filehandle, "raw", n);
+  return(iconv(rawToChar(txt[txt != as.raw(0)]), to = to));
+}
--- README.md
+++ README.md
@@ -5,13 +5,17 @@ Read bitmap images in WAL file format in R.
 
 The WAL file format is an old format for storing indexed bitmap images, used in idtech1 and idtech2 games. The WAL files that hold the Quake 2 textures are stored inside Quake pak file archives (similar to zip format), so you will not find any in your Quake 2 directory unless you extracted them for mapping or modding.
 
+
 ## Package API
 
-This is WIP.
+Not yet, this is WIP. The relevant function is `wal::read.wal()` though.
+
 
 ## References
 
-Documentation on the WAL format seems sparse. I did not find an official spec so I used the Quake 2 source code to learn about the format. Relevant files are:
+Documentation on the WAL format seems sparse. I did not find any official spec so I used the Quake 2 source code to learn about the format. Relevant files are:
 
 * [r_image.c](https://github.com/id-Software/Quake-2/blob/master/ref_soft/r_image.c) with WAL loading function `R_LoadWal`
-* To understand the latter, you will need to have a look at the `miptex_s` struct in [qcommon/qfiles.h](https://github.com/id-Software/Quake-2/blob/master/qcommon/qfiles.h)
+* To understand the latter, you will need to have a look at the `miptex_s` struct in [qfiles.h](https://github.com/id-Software/Quake-2/blob/master/qcommon/qfiles.h) and at the `image_s` struct in [r_local.h](https://github.com/id-Software/Quake-2/blob/master/ref_soft/r_local.h)
+
+As such, this package reads *what Quake 2 considers* the WAL format.
