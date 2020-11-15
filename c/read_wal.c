b2e4f5553b5928bb9ed919586126ebf710d98e61
Tim Schäfer <ts+code@rcmd.org>
2020-11-15 16:39:25 +0100

    read data in C example, add info in header

--- c/read_wal.c
+++ c/read_wal.c
@@ -1,3 +1,14 @@
+// read_wal.c -- read a WAL image file in C.
+// This file is distributed as part of the 'wal' R package repo and is covered by its license.
+//
+// Compilation and usage:
+//
+//    gcc read_wal.c
+//    ./a.out path/to/some_image.wal
+//
+// Written by Tim Schaefer, 2020-11-14.
+
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -38,7 +49,13 @@ int main(int argc, char *argv[]) {
     printf("The contents is %d\n", contents);
     printf("The value is %d\n", value);
 
-    // Could read data here.
+    // Read image data. Each entry represents a pixel and is an index (in range 0-255) into the color palette, which is not part of the file.
+    // A WAL file contains several image resolutions (mipmaps), the following code reads the first (i.e., the highest-quality) image version.
+    int pixel_data[width * height];
+    fseek(fh, mip_level_offsets[0], SEEK_SET);
+    fread(&pixel_data, sizeof(pixel_data), 1, fh);
+
+    // The pixel data should be reshaped to a matrix of width x height to use it. We don't do that here.
 
     fclose(fh);
 }
