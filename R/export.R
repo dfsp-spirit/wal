51f7542f12904b7d7443feef4ebeffaf8691e44c
Tim Schäfer <ts+code@rcmd.org>
2020-11-18 21:56:09 +0100

    add PNG export support, update docs

--- R/export.R
+++ R/export.R
@@ -5,12 +5,14 @@
 #'
 #' @param filepath character string, path to the JPEG file to write, including the file extension.
 #'
+#' @param ... extra parameters passed to \code{jpeg::writeJPEG}. Can be used to set JPEG quality.
+#'
 #' @inheritParams read.wal
 #'
 #' @importFrom jpeg writeJPEG
 #' @importFrom freesurferformats rotate3D flip3D
 #' @export
-wal.export.to.jpeg <- function(wal, filepath, apply_palette = wal::pal_q2()) {
+wal.export.to.jpeg <- function(wal, filepath, apply_palette = wal::pal_q2(), ...) {
   if(! is.character(filepath)) {
     stop("Parameter 'filepath' must be a character string.");
   }
@@ -19,14 +21,6 @@ wal.export.to.jpeg <- function(wal, filepath, apply_palette = wal::pal_q2()) {
   }
   check.palette(apply_palette);
 
-  #channel_red = matrix(apply_palette[wal$raw_data, 1], nrow = wal$header$height, byrow = F);
-  #channel_green = matrix(apply_palette[wal$raw_data, 2], nrow = wal$header$height, byrow = F);
-  #channel_blue = matrix(apply_palette[wal$raw_data, 3], nrow = wal$header$height, byrow = F);
-
-  #channel_red = apply(channel_red, 2, rev);
-  #channel_green = apply(channel_green, 2, rev);
-  #channel_blue = apply(channel_blue, 2, rev);
-
   channel_red = apply_palette[wal$raw_data, 1];
   channel_green = apply_palette[wal$raw_data, 2];
   channel_blue = apply_palette[wal$raw_data, 3];
@@ -34,5 +28,38 @@ wal.export.to.jpeg <- function(wal, filepath, apply_palette = wal::pal_q2()) {
   jpeg_img = array( (c( channel_red , channel_green, channel_blue )) , dim = c( wal$header$width, wal$header$height, 3)) / 255.;
   jpeg_img = freesurferformats::rotate3D(jpeg_img, 3, 90)
   jpeg_img = freesurferformats::flip3D(jpeg_img, 1, "horizontally")
-  jpeg::writeJPEG(jpeg_img, target = filepath);
+  jpeg::writeJPEG(jpeg_img, target = filepath, ...);
 }
+
+#' @title Export wal instance to PNG format image file.
+#'
+#' @param wal a wal instance, as returned by \code{read.wal}
+#'
+#' @param filepath character string, path to the PNG file to write, including the file extension.
+#'
+#' @param ... extra parameters passed to \code{png::writePNG}.
+#'
+#' @inheritParams read.wal
+#'
+#' @importFrom png writePNG
+#' @importFrom freesurferformats rotate3D flip3D
+#' @export
+wal.export.to.png <- function(wal, filepath, apply_palette = wal::pal_q2(), ...) {
+  if(! is.character(filepath)) {
+    stop("Parameter 'filepath' must be a character string.");
+  }
+  if(is.null(apply_palette)) {
+    stop("Paramter 'palette' must not be NULL");
+  }
+  check.palette(apply_palette);
+
+  channel_red = apply_palette[wal$raw_data, 1];
+  channel_green = apply_palette[wal$raw_data, 2];
+  channel_blue = apply_palette[wal$raw_data, 3];
+
+  png_img = array( (c( channel_red , channel_green, channel_blue )) , dim = c( wal$header$width, wal$header$height, 3)) / 255.;
+  png_img = freesurferformats::rotate3D(png_img, 3, 90)
+  png_img = freesurferformats::flip3D(png_img, 1, "horizontally")
+  png::writePNG(png_img, target = filepath, ...);
+}
+
