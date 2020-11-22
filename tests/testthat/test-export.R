cf5ef338efbb0351a58ebfee738684a006532a3e
Tim Schäfer <ts+code@rcmd.org>
2020-11-22 22:11:59 +0100

    expand vignette, add test

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -8,3 +8,14 @@ testthat::test_that("We can convert a WAL file to JPEG format.", {
   tex_jpg = tempfile(fileext = ".jpg");
   wal::wal.export.to.jpeg(wal, tex_jpg);
 })
+
+
+testthat::test_that("We can convert a WAL file to PNG format.", {
+
+  walf = system.file("extdata", "bricks.wal", package = "wal", mustWork = TRUE);
+
+  wal = wal::read.wal(walf);
+  tex_png = tempfile(fileext = ".png");
+  wal::wal.export.to.png(wal, tex_png);
+})
+
